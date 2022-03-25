aa3944c5ccc4d3c4b019dd4bf042ec14a9c03f99
Kayo Costa <kayo220@users.noreply.github.com>
2022-03-25 19:07:05 -0300

    remove commonMiddleware from createAuction handler

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,33 +1,26 @@
 import { v4 as uuid } from 'uuid';
 import AWS from 'aws-sdk';
 import { commonMiddleware } from '../lib/commonMiddleware';
-import { createError } from 'http-errors';
 
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 async function createAuction(event, context) {
 
-  try {
-    const { title } = JSON.parse(event.body);
-    const now = new Date()
+  const { title } = JSON.parse(event.body);
+  const now = new Date()
 
-    const auction = {
-      id: uuid(),
-      title,
-      status: 'OPEN',
-      createdAt: now.toISOString()
-    }
-
-    await dynamodb.put({
-      TableName: process.env.AUCTIONS_TABLE_NAME,
-      Item: auction
-    }).promise();
-  } catch (error) {
-
-    console.error(error)
-    throw new createError.InternalServerError(error);
+  const auction = {
+    id: uuid(),
+    title,
+    status: 'OPEN',
+    createdAt: now.toISOString()
   }
+
+  await dynamodb.put({
+    TableName: process.env.AUCTIONS_TABLE_NAME,
+    Item: auction
+  }).promise();
 
   return {
     statusCode: 201,
